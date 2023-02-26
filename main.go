c8e99c1c429795425a7a5cbebae0c8af5224b210
Ikhlash Mulyanurahman <nurahmanmulya@gmail.com>
2023-02-26 10:42:13 +0700

    add source code goa lang full

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "latihan-web/controllers"
+)
+
+func main() {
+    
+    mux := http.NewServeMux()
+    
+    mux.HandleFunc("/", controllers.Home)
+    mux.HandleFunc("/about", controllers.About)
+    mux.HandleFunc("/form", controllers.Form)
+    mux.HandleFunc("/message", controllers.Message)
+    
+    server := http.Server{
+        Addr: "localhost:8080", 
+        Handler: mux, 
+    }
+    
+    log.Println("Menjalankan Web Server...")
+    err := server.ListenAndServe()
+    log.Fatal(err)
+}
